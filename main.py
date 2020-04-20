8f8b38fd62b57ba7108f55846d59d2a984f42b8d
Abhinav-github <singh.avi99@gmail.com>
2020-04-20 17:09:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 # Python program to read image using OpenCV
 import cv2
 import imread
+import numpy
+import time
 # Save image in set directory
 # Read RGB image
-RGB = cv2.imread('pic.jpg');
-print(RGB[0])
+txt_parts = {}
+i = 0
+with open('pic.jpg', 'rb') as pic:
+    BRG = cv2.imread('pic.jpg')
+#    print(BRG)
+#    time.sleep(3)
+#    lower = numpy.array([242, 242, 242])
+#    upper = numpy.array([255, 255, 255])
+#    shapeMask = cv2.inRange(BRG, lower, upper)
+#    cv2.imshow("Mask", shapeMask)
+#    time.sleep(3)
+    for i in range(len(BRG)):
+        for j in range(len(BRG[0])):
+            if BRG[i][j][0] == 0 and BRG[i][j][1] == 0 and BRG[i][j][2] == 0:
+                txt_parts[(i,j)] = True;
+print(txt_parts)
